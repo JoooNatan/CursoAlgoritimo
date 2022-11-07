57d2f026bafe6578f08ea4d423aa6985ce75e468
João Natan <joaonatan67548@gmail.com>
2022-11-07 16:06:42 -0300

    Pa

--- ListaExercicios/Ex69.java
+++ ListaExercicios/Ex69.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Ex69 {
     public static void main(String[] args) {
-        
+        int termo, razao, primeiro, soma = 0;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro termo: ");
+        primeiro = scan.nextInt();
+        System.out.print("Digite a razão: ");
+        razao = scan.nextInt();
+        termo = primeiro;
+
+        for(int cont = 1; cont <= 10; cont++) {
+            System.out.printf("%d ", termo);
+            termo += razao;
+            soma += termo;
+        }
+
+        System.out.printf("\nSoma dos termos = %d", soma);
     }
 }
